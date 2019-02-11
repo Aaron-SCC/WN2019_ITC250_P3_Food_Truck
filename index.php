c90fb2c46e04e65f3974a8b135a0fb4f3a9e59a1
Liyun Cecil <liyuncecil@gmail.com>
2019-02-10 22:45:31 -0800

    updates index.php

--- index.php
+++ index.php
@@ -1,34 +1,11 @@
 <?php include 'includes/header.php'; ?>
+<?php include 'includes/items.php'; ?>
+
 <?php
 //index.php
-$myItem = new Item(1, 'Taco', 'Our tacos are awesome', 4.95);
-/*$myItem->addExtra('sour cream');
-$myItem->addExtra('cheese');
-$myItem->addExtra('guacamole');
-$myItem->addExtra('salsa');
-//out go to bag of food items*/
-$items[] = $myItem;
-
-
-$myItem = new Item(2, 'Hot Dog', 'Our hot dogs are awesome', 5.95);
-/*$myItem->addExtra('ketchup');
-$myItem->addExtra('onions');
-$myItem->addExtra('spicy mustard');
-$myItem->addExtra('saurkraut');*/
-$items[] = $myItem;
-
-
-$myItem = new Item(3, 'Sundae', 'Our sundaes are awesome', 3.95);
-/*$myItem->addExtra('chocolate');
-$myItem->addExtra('nuts');
-$myItem->addExtra('whipped cream');
-$myItem->addExtra('cherry');*/
-$items[] = $myItem;
-
 
 $cost = 0;
 $number_of_items = 0;
-
 foreach($items as $item) 
 {
     $cost += $item->Price;
@@ -39,34 +16,8 @@ foreach($items as $item)
           <td>$item->Name</td>
           <td>$item->Description</td>
           <td>$item->Price</td>
-          </tr>";  
-}
-
-class Item{
-    //properties
-    public $ID = 0;
-    public $Name = '';
-    public $Description = '';
-    public $Price = 0;
-    public $Extras = array(); //presents 1 to many relationships
-    
-    //constructor
-    public function __construct($ID, $Name, $Description, $Price){
-        //$this->ID points to the variable $ID
-        $this->ID = $ID;
-        $this->Name = $Name;
-        $this->Description = $Description;
-        $this->Price = $Price;
-    }//end constructor
-    
-    //methods
-    public function addExtra($extra){
-        
-        $this->Extras[] = $extra;
-        
-    }//end function addExtra();
-}//end Item class
+          </tr>";
+    }
 ?>
-
             
 <?php include 'includes/footer.php'; ?>           
