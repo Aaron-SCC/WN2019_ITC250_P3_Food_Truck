ba21fa75e2cebf1e263433d6407b50c5a96c228f
Liyun Cecil <liyuncecil@gmail.com>
2019-02-10 16:51:18 -0800

    creates basic visual design

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Food Truck</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!--add css file-->
+    <link rel="stylesheet" href="css/bootstrap.css">
+  </head>
+  <body>
+    <div id="page">
+      <h1 align="center">Food Truck</h1>
+      <h3 align="center">You know you're hungry, order now!</h3>
+    <div class="container">
+      <h3 align="center">Menu</h3>
+      <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Description</th>
+              <th scope="col">Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="table-success">
+              <td>Taco</td>
+              <td>Our tacos are awesome</td>
+              <td>$4.95</td>
+            </tr>
+          </tbody>
+      </table>        
+    </div>
+    </div>
+  </body>
+</html>
