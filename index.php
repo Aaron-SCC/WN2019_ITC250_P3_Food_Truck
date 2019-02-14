a997aa2926420eed682a980d2c0456d39467607c
zjwda2016 <zjwda.2016@gmail.com>
2019-02-14 09:20:30 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php include 'includes/header.php'; ?>
-<?php include 'includes/items.php'; ?>
 
 <?php
 //index.php
-
 $cost = 0;
 $number_of_items = 0;
 foreach($items as $item) 
@@ -11,13 +9,34 @@ foreach($items as $item)
     $cost += $item->Price;
     $number_of_items++;
     //loop through the data
-
-    echo "<tr>
-          <td>$item->Name</td>
-          <td>$item->Description</td>
-          <td>$item->Price</td>
-          </tr>";
-    }
+?>
+    	  <tr>
+          <form method='post'>
+          <input type='hidden' name='id' value='<?php echo $item->ID; ?>' />
+            
+          <input type='hidden' name='name' value='<?php echo $item->Name; ?>' />
+          <td><?php echo $item->Name; ?></td>
+            
+          <input type='hidden' name='description' value='<?php echo $item->Description; ?>' />
+          <td><?php echo $item->Description; ?></td>
+          
+		  <input type='hidden' name='price' value='<?php echo $item->Price; ?>' />
+          <td><?php echo $item->Price; ?></td>
+            
+          <td><?php echo $item->Inventory; ?></td>
+          
+          <td>
+          	<input type='text' name='quantity' value='1' class='form-control' />
+          </td>
+          
+          
+          <td>
+         	<input type='submit' name='addToCart' style='margin-top:5px;' class='btn btn-success' value='Add to Cart' />
+          </td>
+          </form>
+          </tr>
+<?php
+}
 ?>
             
-<?php include 'includes/footer.php'; ?>           
+<?php include 'includes/footer.php'; ?>
