89efc102e6d6cea3a0a713074d1173b1b9a5bbd6
zjwda2016 <zjwda.2016@gmail.com>
2019-02-14 09:21:04 -0800

    Update items.php

--- includes/items.php
+++ includes/items.php
@@ -1,7 +1,7 @@
 <?php
 //index.php
 
-$myItem = new Item(1, 'Taco', 'Our tacos are awesome', 4.95);
+$myItem = new Item(1, 'Taco', 'Our tacos are awesome', 4.95, 10);
 /*$myItem->addExtra('sour cream');
 $myItem->addExtra('cheese');
 $myItem->addExtra('guacamole');
@@ -9,14 +9,14 @@ $myItem->addExtra('salsa');
 //out go to bag of food items*/
 $items[] = $myItem;
 
-$myItem = new Item(2, 'Hot Dog', 'Our hot dogs are awesome', 5.95);
+$myItem = new Item(2, 'Hot Dog', 'Our hot dogs are awesome', 5.95, 20);
 /*$myItem->addExtra('ketchup');
 $myItem->addExtra('onions');
 $myItem->addExtra('spicy mustard');
 $myItem->addExtra('saurkraut');*/
 $items[] = $myItem;
 
-$myItem = new Item(3, 'Sundae', 'Our sundaes are awesome', 3.95);
+$myItem = new Item(3, 'Sundae', 'Our sundaes are awesome', 3.95, 25);
 /*$myItem->addExtra('chocolate');
 $myItem->addExtra('nuts');
 $myItem->addExtra('whipped cream');
@@ -29,15 +29,17 @@ class Item{
     public $Name = '';
     public $Description = '';
     public $Price = 0;
+  	public $Inventory = 0;
     public $Extras = array(); //presents 1 to many relationships
     
     //constructor
-    public function __construct($ID, $Name, $Description, $Price){
+    public function __construct($ID, $Name, $Description, $Price, $Inventory){
         //$this->ID points to the variable $ID
         $this->ID = $ID;
         $this->Name = $Name;
         $this->Description = $Description;
         $this->Price = $Price;
+      	$this->Inventory = $Inventory;
     }//end constructor
     
     //methods
@@ -48,5 +50,3 @@ class Item{
     }//end function addExtra();
 }//end Item class
 ?>
-
-        
