b5c94fd822c07123ea85ab18ab04f2fa788fe0a9
Aaron-SCC <32554879+Aaron-SCC@users.noreply.github.com>
2019-02-20 21:25:16 -0800

    corrected prices to have 2 digits after point
    Per Bill's review in class on 2019_02_19
    corrected prices to have 2 digits after decimal point for the extras

--- includes/items.php
+++ includes/items.php
@@ -11,29 +11,28 @@
  * @link http://derekheducation.dreamhosters.com/p3/index.php
  * @license http://opensource.org/licenses/osl-3.0.php Open Software License ("OSL") v. 3.0
  */
-
 // Item(id for item, name, description, price, quantity);
 $myItem = new Item(1, 'Taco', 'Our tacos are awesome', 4.95, 10);
 // addExtra(id fpr exxtra, extra name, extra price);
-$myItem->addExtra(1, 'Sour Cream', 0.5);
-$myItem->addExtra(2, 'Cheese', 0.3);
-$myItem->addExtra(3, 'Guacamole', 0.6);
-$myItem->addExtra(4, 'Salsa', 0.8);
+$myItem->addExtra(1, 'Sour Cream', 0.51);
+$myItem->addExtra(2, 'Cheese', 0.32);
+$myItem->addExtra(3, 'Guacamole', 0.63);
+$myItem->addExtra(4, 'Salsa', 0.84);
 //out go to bag of food items
 $items[] = $myItem;
 
 $myItem = new Item(2, 'Hot Dog', 'Our hot dogs are awesome', 5.95, 20);
-$myItem->addExtra(1, 'Ketchup', 0.7);
-$myItem->addExtra(2, 'Onions', 0.3);
-$myItem->addExtra(3, 'Spicy mustard', 0.9);
-$myItem->addExtra(4, 'Saurkraut', 0.5);
+$myItem->addExtra(1, 'Ketchup', 0.75);
+$myItem->addExtra(2, 'Onions', 0.36);
+$myItem->addExtra(3, 'Spicy mustard', 0.97);
+$myItem->addExtra(4, 'Saurkraut', 0.58);
 $items[] = $myItem;
 
 $myItem = new Item(3, 'Sundae', 'Our sundaes are awesome', 3.95, 25);
-$myItem->addExtra(1, 'Chocolate', 1.3);
-$myItem->addExtra(2, 'Nuts', 2.1);
-$myItem->addExtra(3, 'Whipped cream', 3.2);
-$myItem->addExtra(4, 'Cherry', 1.7);
+$myItem->addExtra(1, 'Chocolate', 1.39);
+$myItem->addExtra(2, 'Nuts', 2.17);
+$myItem->addExtra(3, 'Whipped cream', 3.25);
+$myItem->addExtra(4, 'Cherry', 1.73);
 $items[] = $myItem;
 
 class Item
